28b88b3a1764900b0bcfba18eb9cad205e93a296
Kaleb <kaleb@Kalebs-iMac.local>
2026-01-04 15:24:59 -0600

    Update Meta Pixel ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,13 +53,13 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1365605978180120');
+            fbq('init', '889248227046303');
             fbq('track', 'PageView');
           `}
         </Script>
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=1365605978180120&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=889248227046303&ev=PageView&noscript=1"
           />
         </noscript>
 
